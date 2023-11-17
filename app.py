50f22ac76ea86e01048bc2617b25a8054b447f76
t0f <toffelaakso@gmail.com>
2023-11-17 17:58:04 +0200

    initial db connection

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -13,6 +18,15 @@ def login():
 @app.route("/add_item")
 def add_item():
     return render_template("add_item.html")
+
+def addBadge():
+    return
+
+def removeBadge():
+    return
+
+def updateBadge():
+    return
 
 
 if __name__ == "__main__":
