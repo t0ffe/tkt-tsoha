8c286d34d85ea75e44b433d4a2a59bbcd9a6bce8
t0f <toffelaakso@gmail.com>
2023-11-19 21:01:59 +0200

    made schemas clearer

--- schema.sql
+++ schema.sql
@@ -3,24 +3,23 @@ CREATE TABLE IF NOT EXISTS users (
     name TEXT,
     password TEXT,
     role INTEGER,
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    student_organization INTEGER
+
 );
 
-CREATE TABLE IF NOT EXISTS collections (
+CREATE TABLE IF NOT EXISTS student_organization (
     id SERIAL PRIMARY KEY,
-    creator_id INTEGER REFERENCES users,
-    name TEXT,
-    visible INTEGER,
-    created_at TIMESTAMP
+    name TEXT
 );
 
 CREATE TABLE IF NOT EXISTS badges (
     id SERIAL PRIMARY KEY,
-    collection_id INTEGER REFERENCES collections,
     amount INTEGER,
     price numeric(7,4),
     name TEXT,
     designer TEXT,
     supplier TEXT,
-    created_at TIMESTAMP
+    created_at TIMESTAMP,
+    student_organization INTEGER
 );
